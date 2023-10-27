59e9010dd9e19cbe4bcb62846be8475d2a2e2ae7
Emrah Dulan <emrah.dulan@outlook.com>
2023-10-27 19:56:49 +0200

    first commit

---
+++ store/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
+from django.urls import reverse
+
+# Create your models here.
+
+class ProductManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse("store:category_list", args=[self.slug])
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='product_creator', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
+
+    class Meta:
+        verbose_name_plural = 'products'
+        ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse("store:product_detail", args=[self.slug])
+    
+
+    def __str__(self) -> str:
+        return self.name
---
+++ store/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Category, Product
+
+# Create your views here.
+
+def product_all(request):
+    products = Product.products.all()
+
+    return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+
+    return render(request, 'store/products/product.html', {'product': product})
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.products.filter(category=category)
+    return render(request, 'store/products/category.html', {'category': category, 'products': products})
