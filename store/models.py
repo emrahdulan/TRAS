1cd25dabd5ad584ffe1ead11539ccca36b34cfa8
Emrah Dulan <emrah.dulan@outlook.com>
2023-10-27 22:57:06 +0200

    ui commit

--- store/models.py
+++ store/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     created_by = models.ForeignKey(User, related_name='product_creator', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     is_active = models.BooleanField(default=True)
